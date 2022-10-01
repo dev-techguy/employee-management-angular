86286b3363e508ece4b36850eb8b459ebc1971f0
dev-techguy <dev.techguy@gmail.com>
2022-10-01 15:26:20 +0300

    :alien: Updating code due to external API changes.

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,31 +1,32 @@
-import { Injectable } from "@angular/core";
-import { Employee } from "./employee";
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+import { environment } from 'src/environments/environment';
 
-
-@Injectable({
-  providedIn: 'root'
-})
-
+@Injectable({ providedIn: 'root' })
 export class EmployeeService {
-  private apiServiceUrl = 'http://127.0.0.1:8081/api/v1/employees';
+  private apiServerUrl = `${environment.apiBaseUrl}/api/v1/employees`;
 
   constructor(private http: HttpClient) { }
 
   public getEmployees(): Observable<Employee[]> {
-    return this.http.get<Employee[]>('${this.apiServiceUrl}');
+    return this.http.get<Employee[]>(`${this.apiServerUrl}`);
   }
 
-  public addEmployee(employee: Employee): Observable<Employee[]> {
-    return this.http.post<Employee[]>('${this.apiServiceUrl}', employee);
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiServerUrl}`, employee);
   }
 
-  public updateEmployee(employee: Employee): Observable<Employee[]> {
-    return this.http.put<Employee[]>('${this.apiServiceUrl}', employee);
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServerUrl}`, employee);
+  }
+
+  public getEmployee(employeeId: number): Observable<Employee> {
+    return this.http.delete<Employee>(`${this.apiServerUrl}/${employeeId}`);
   }
 
   public deleteEmployee(employeeId: number): Observable<void> {
-    return this.http.delete<void>('${this.apiServiceUrl}/${employeeId}');
+    return this.http.delete<void>(`${this.apiServerUrl}/${employeeId}`);
   }
 }
